8dfb5333bb65f7c37fea83d50f97944c58780a82
DAVolgas <volgas1974@gmail.com>
2024-07-11 01:31:01 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,7 +1,16 @@
-with atribution_showcase as (
+with last_paid_visits as (
     select
-        s.visitor_id,
-        s.visit_date,
+        visitor_id,
+        MAX(visit_date) as last_paid_click_date
+    from sessions
+    where medium != 'organic'
+    group by 1
+),
+
+atribution_showcase as (
+    select
+        lp.visitor_id,
+        lp.last_paid_click_date as visit_date,
         s."source" as utm_source,
         s.medium as utm_medium,
         s.campaign as utm_campaign,
@@ -10,38 +19,23 @@ with atribution_showcase as (
         l.amount,
         l.closing_reason,
         l.status_id
-    from sessions as s
-    left join leads as l
+    from last_paid_visits as lp
+    left join sessions as s
         on
-            s.visitor_id = l.visitor_id
-    where s.medium != 'organic'
+            lp.visitor_id = s.visitor_id
+            and lp.last_paid_click_date = s.visit_date
+            and s.medium != 'organic'
+    left join leads as l
+        on s.visitor_id = l.visitor_id
     order by 8 desc nulls last, 2, 3, 4, 5
-),
-
-last_paid_visits as (
-    select
-        visitor_id,
-        MAX(visit_date) as last_paid_click_date
-    from atribution_showcase
-    where utm_medium != 'organic'
-    group by 1
 )
 
 select
-    a.visitor_id,
-    a.lead_id,
-    a.created_at,
-    a.amount,
-    a.closing_reason,
-    a.status_id,
-    l.last_paid_click_date,
-    a.utm_source,
-    a.utm_medium,
-    a.utm_campaign
-from atribution_showcase as a
-inner join last_paid_visits as l
-    on a.visitor_id = l.visitor_id
-where
-    a.created_at is not null
-    and a.created_at >= l.last_paid_click_date;
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    COUNT(distinct lead_id) as leads_count
+from atribution_showcase
+group by 1, 2, 3
+order by 4 DESC;
 	
