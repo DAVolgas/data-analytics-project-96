a09cec3c05179f7a3df0a20f6858bc5b907b9c1a
DAVolgas <volgas1974@gmail.com>
2024-08-20 16:05:46 +0300

    Add files via upload
    refactor code in dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -57,7 +57,41 @@ WHERE s.medium != 'organic'
 ORDER BY 8 DESC NULLS LAST, 2, 3, 4, 5;
 
 -- срок, через который закрывается 90% лидов
--- запрос к витрине lpc
+WITH last_paid_visits AS (
+    SELECT
+        visitor_id,
+        max(visit_date) AS last_paid_click_date
+    FROM sessions
+    WHERE medium != 'organic'
+    GROUP BY 1
+),
+
+show_case_lpc AS (
+    SELECT
+        lp.visitor_id,
+        lp.last_paid_click_date AS visit_date,
+        s."source" AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        l.created_at::DATE - visit_date::DATE AS intervals
+    FROM sessions AS s
+    INNER JOIN last_paid_visits AS lp
+        ON
+            s.visitor_id = lp.visitor_id
+            AND s.visit_date = lp.last_paid_click_date
+    LEFT JOIN leads AS l
+        ON
+            s.visitor_id = l.visitor_id
+            AND s.visit_date <= l.created_at
+    WHERE s.medium != 'organic'
+    ORDER BY 8 DESC NULLS LAST, 2, 3, 4, 5
+)
+
 SELECT
     percentile_disc(0.9) WITHIN GROUP
     (ORDER BY intervals) AS leads_close_interval
@@ -72,9 +106,8 @@ WITH vy_ads_cost AS (
         utm_source,
         utm_medium,
         utm_campaign,
-        sum(daily_spent) AS total_cost
+        daily_spent
     FROM vk_ads
-    GROUP BY 1, 2, 3, 4
 
     UNION
 
@@ -83,124 +116,160 @@ WITH vy_ads_cost AS (
         utm_source,
         utm_medium,
         utm_campaign,
-        sum(daily_spent) AS total_cost
+        daily_spent
     FROM ya_ads
-    GROUP BY 1, 2, 3, 4
 ),
 
-purchases AS (
+total_cost AS (
     SELECT
-        visitor_id,
-        lead_id,
-        amount
-    FROM leads
-    WHERE status_id = 142
+        campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) AS total_cost
+    FROM vy_ads_cost
+    GROUP BY 1, 2, 3, 4
 ),
 
 last_visit AS (
     SELECT
         visitor_id,
-        max(visit_date) AS last_paid_click_date
+        max(visit_date) as last_paid_click_date
     FROM sessions
     WHERE medium != 'organic'
     GROUP BY 1
 ),
 
-showcase AS (
+total_amount AS (
     SELECT
         lv.last_paid_click_date::DATE AS visit_date,
         s.source AS utm_source,
         s.medium AS utm_medium,
         s.campaign AS utm_campaign,
-        vy.total_cost,
-        count(DISTINCT s.visitor_id) AS visitors_count,
-        count(DISTINCT l.lead_id) AS leads_count,
-        count(p.lead_id) AS purchases_count,
-        sum(p.amount) AS revenue
+        count(distinct s.visitor_id) AS visitors_count,
+        count(distinct l.lead_id) AS leads_count,
+        count(CASE
+            WHEN l.status_id = 142 THEN 1
+        END)
+        AS purchases_count,
+        sum(l.amount) AS revenue
     FROM last_visit AS lv
     INNER JOIN sessions AS s
         ON
             lv.visitor_id = s.visitor_id
             AND lv.last_paid_click_date = s.visit_date
-    LEFT JOIN vy_ads_cost AS vy
-        ON
-            lv.last_paid_click_date::DATE = vy.campaign_date
-            AND s.source = vy.utm_source
-            AND s.medium = vy.utm_medium
-            AND s.campaign = vy.utm_campaign
     LEFT JOIN leads AS l
         ON
             lv.last_paid_click_date <= l.created_at
             AND lv.visitor_id = l.visitor_id
-    LEFT JOIN purchases AS p
-        ON
-            lv.visitor_id = p.visitor_id
-            AND l.lead_id = p.lead_id
-    GROUP BY 1, 2, 3, 4, 5
-    ORDER BY 9 DESC NULLS LAST, 1, 2 DESC, 3, 4, 5
+    GROUP BY 1, 2, 3, 4
 )
 
 SELECT
-    visit_date,
-    visitors_count,
-    utm_source,
-    utm_medium,
-    utm_campaign,
-    total_cost,
-    leads_count,
-    purchases_count,
-    revenue
-FROM showcase;
+    ta.visit_date,
+    ta.visitors_count,
+    ta.utm_source,
+    ta.utm_medium,
+    ta.utm_campaign,
+    tc.total_cost,
+    ta.leads_count,
+    ta.purchases_count,
+    ta.revenue
+FROM total_amount AS ta
+LEFT JOIN total_cost AS tc
+    ON
+        ta.visit_date = tc.campaign_date
+        AND ta.utm_source = tc.utm_source
+        AND ta.utm_medium = tc.utm_medium
+        AND ta.utm_campaign = tc.utm_campaign
+ORDER BY 9 DESC NULLS LAST, 1, 2 DESC, 3, 4, 5;
 
--- для общего графика по окупаемости рекламы
--- запрос к витрине aggregate lpc
+-- для итоговых метрик и общего графика по окупаемости рекламы
+WITH vy_ads_cost AS (
+    SELECT
+        campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        daily_spent
+    FROM vk_ads
+
+    UNION
+
+    SELECT
+        campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        daily_spent
+    FROM ya_ads
+),
+
+total_cost AS (
+    SELECT
+        campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) AS total_cost
+    FROM vy_ads_cost
+    GROUP BY 1, 2, 3, 4
+),
+
+last_visit AS (
+    SELECT
+        visitor_id,
+        max(visit_date) as last_paid_click_date
+    FROM sessions
+    WHERE medium != 'organic'
+    GROUP BY 1
+),
+
+total_amount AS (
+    SELECT
+        lv.last_paid_click_date::DATE AS visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        count(distinct s.visitor_id) AS visitors_count,
+        count(distinct l.lead_id) AS leads_count,
+        count(CASE
+            WHEN l.status_id = 142 THEN 1
+        END)
+        AS purchases_count,
+        sum(l.amount) AS revenue
+    FROM last_visit AS lv
+    INNER JOIN sessions AS s
+        ON
+            lv.visitor_id = s.visitor_id
+            AND lv.last_paid_click_date = s.visit_date
+    LEFT JOIN leads AS l
+        ON
+            lv.last_paid_click_date <= l.created_at
+            AND lv.visitor_id = l.visitor_id
+    GROUP BY 1, 2, 3, 4
+),
+showcase AS (
 SELECT
-    utm_source,
-    sum(total_cost) AS total_cost,
-    sum(revenue) AS revenue,
-    round((sum(revenue) - sum(total_cost)) * 100.0 / sum(total_cost), 2) AS roi
-FROM showcase
-WHERE total_cost > 0
-GROUP BY 1
-ORDER BY 2 DESC;
+    ta.visit_date,
+    ta.visitors_count,
+    ta.utm_source,
+    ta.utm_medium,
+    ta.utm_campaign,
+    tc.total_cost,
+    ta.leads_count,
+    ta.purchases_count,
+    ta.revenue
+FROM total_amount AS ta
+LEFT JOIN total_cost AS tc
+    ON
+        ta.visit_date = tc.campaign_date
+        AND ta.utm_source = tc.utm_source
+        AND ta.utm_medium = tc.utm_medium
+        AND ta.utm_campaign = tc.utm_campaign
+ORDER BY 9 DESC NULLS LAST, 1, 2 DESC, 3, 4, 5
+)
 
--- для итоговой таблицы с метриками по utm_source
--- запрос к витрине aggregate lpc
-SELECT
-    visit_date::DATE,
-    utm_source,
-    sum(total_cost) AS total_cost,
-    coalesce(sum(revenue), 0) AS revenue,
-    sum(visitors_count) AS visitors_count,
-    sum(leads_count) AS leads_count,
-    sum(purchases_count) AS purchases_count,
-    round(sum(total_cost) / sum(visitors_count), 2) AS cpu,
-    CASE
-        WHEN
-            sum(leads_count) > 0
-            THEN round(sum(total_cost) / sum(leads_count), 2)
-    END AS cpl,
-    CASE
-        WHEN
-            sum(purchases_count) > 0
-            THEN round(sum(total_cost) / sum(purchases_count), 2)
-    END AS cppu,
-    round(
-        (coalesce(sum(revenue), 0) - sum(total_cost)) * 100.0 / sum(total_cost),
-        2
-    ) AS roi,
-    CASE
-        WHEN
-            sum(purchases_count) > 0
-            THEN round(sum(revenue) / sum(purchases_count))
-    END AS avg_amount
-FROM showcase
-WHERE total_cost > 0
-GROUP BY 1, 2
-ORDER BY 3 DESC;
-
--- аналогичный предыдущему запрос с метриками по всем utm
--- запрос к витрине aggregate lpc
 SELECT
     utm_source,
     utm_medium,
@@ -243,7 +312,91 @@ ORDER BY 4 DESC;
 
 -- конверсия клик-лид-покупка
 -- аналогичные запросы с фильтром по vk и yandex для этих источников
--- запрос к витрине aggregate lpc 
+WITH vy_ads_cost AS (
+    SELECT
+        campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        daily_spent
+    FROM vk_ads
+
+    UNION
+
+    SELECT
+        campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        daily_spent
+    FROM ya_ads
+),
+
+total_cost AS (
+    SELECT
+        campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) AS total_cost
+    FROM vy_ads_cost
+    GROUP BY 1, 2, 3, 4
+),
+
+last_visit AS (
+    SELECT
+        visitor_id,
+        max(visit_date) as last_paid_click_date
+    FROM sessions
+    WHERE medium != 'organic'
+    GROUP BY 1
+),
+
+total_amount AS (
+    SELECT
+        lv.last_paid_click_date::DATE AS visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        count(distinct s.visitor_id) AS visitors_count,
+        count(distinct l.lead_id) AS leads_count,
+        count(CASE
+            WHEN l.status_id = 142 THEN 1
+        END)
+        AS purchases_count,
+        sum(l.amount) AS revenue
+    FROM last_visit AS lv
+    INNER JOIN sessions AS s
+        ON
+            lv.visitor_id = s.visitor_id
+            AND lv.last_paid_click_date = s.visit_date
+    LEFT JOIN leads AS l
+        ON
+            lv.last_paid_click_date <= l.created_at
+            AND lv.visitor_id = l.visitor_id
+    GROUP BY 1, 2, 3, 4
+),
+showcase AS (
+SELECT
+    ta.visit_date,
+    ta.visitors_count,
+    ta.utm_source,
+    ta.utm_medium,
+    ta.utm_campaign,
+    tc.total_cost,
+    ta.leads_count,
+    ta.purchases_count,
+    ta.revenue
+FROM total_amount AS ta
+LEFT JOIN total_cost AS tc
+    ON
+        ta.visit_date = tc.campaign_date
+        AND ta.utm_source = tc.utm_source
+        AND ta.utm_medium = tc.utm_medium
+        AND ta.utm_campaign = tc.utm_campaign
+ORDER BY 9 DESC NULLS LAST, 1, 2 DESC, 3, 4, 5
+)
+
 SELECT
     sum(visitors_count) AS counting,
     CASE WHEN sum(visitors_count) = sum(visitors_count) THEN 'clicks'
